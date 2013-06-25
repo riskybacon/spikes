87402fc4cb55fac4f0302f9ce24cab303754a64a
Jeff Bowles <jbowles@riskybacon.com>
2013-06-24 19:41:18 -0600

    Removed specular highlight. Not relevant to this example

--- opengl/4.1/shadow_mapping/shadow_fragment.c
+++ opengl/4.1/shadow_mapping/shadow_fragment.c
@@ -1,10 +1,13 @@
 #version 150
 // Shadow mapping with simple Phong shading
 
+in vec4 vertexNormal;
 in vec3 N;
-in vec3 v;
+//in vec3 v;
 in vec2 fragTC;
 in vec4 stPos;
+in vec4 worldPos;
+
 uniform vec4 lightPos;
 uniform sampler2DShadow depthMap;
 
@@ -24,21 +27,27 @@ void main(void)
    // Shadow mapping happens here - attenuate the light if the distance
    // of the occluding object to the light is less than the distance of
    // the receiver to the light.
-   attenuation = occludingDepth < (stPos.z / stPos.w) - 0.00001 ? 0.75 : attenuation;
+   attenuation = occludingDepth < (stPos.z / stPos.w) - 0.0005 ? 0.75 : attenuation;
 
    // Phong shading
    vec4 diffuseMaterial = vec4(0.9, 0.6, 0.5, 1.0);
+   
+#if 0
    vec4 specularMaterial = vec4(1, 0, 0, 1);
    float shininess = 100;
    
-   vec3 E = normalize(-v);
-   vec3 L = normalize(lightPos.xyz - v);
+   vec3 E = normalize(-(worldPos.xyz));
    vec3 R = reflect(-L, N);
    
    float specDP = max(dot(R,E), 0);
    vec4 specular = specularMaterial * pow(specDP, shininess);
+#endif
+   
+   vec3 L = normalize(lightPos.xyz - worldPos.xyz);
    vec4 diffuse = diffuseMaterial * max(dot(N,L), 0);
    
    // Combine Phong shading and shadow mapping light attenuation factor
-   fragColor = clamp(attenuation * (diffuse + specular), 0, 1);
+   //   fragColor = clamp(attenuation * (diffuse + specular), 0, 1);
+   fragColor = clamp(attenuation * diffuse, 0, 1);
+   //   fragColor = vec4(0, 0, N.b, 1);
 }
--- opengl/4.1/shadow_mapping/shadow_vertex.c
+++ opengl/4.1/shadow_mapping/shadow_vertex.c
@@ -7,22 +7,31 @@ in vec4 normal;
 in vec2 tc;
 
 // Transformation matrices
+uniform mat4 model;
 uniform mat4 mvp;
 uniform mat4 invTP;
 uniform mat4 toShadowTex;
 
-out vec3 N;      //< Normal transformed into light space;
-out vec3 v;      //< Position of vertex;
+out vec3 N;      //< Normal transformed
 
 out vec4 stPos;  //< Shadow texture position
 
 out vec2 fragTC; //< Texture coordinate;
+
+out vec4 vertexNormal;
+out vec4 worldPos;
 
 void main(void)
 {
    // Transform vertex into canonical view volume
    gl_Position = mvp      * vertex;
 
+   mat4 itp = transpose(inverse(model));
+   
+   // Get the position of this vertex in world space. This
+   // will be used for lighting
+   worldPos = model * vertex;
+   
    // Transform vertex position to shadow map position
    // Transformations applied
    // model -> world -> light view -> light projection -> [-1,1] -> [0,1]
@@ -33,9 +42,11 @@ void main(void)
 
    // Transform the normal into light space using the inverse
    // transpose of the mvp matrix
-   N = (normalize(invTP * normal)).xyz;
-   v = gl_Position.xyz;
+   N = (normalize(itp * normal)).xyz;
+   //   v = gl_Position.xyz;
    
    // Texture coordinate goes through unchanged
    fragTC = tc;
+   
+   vertexNormal = normal;
 }
