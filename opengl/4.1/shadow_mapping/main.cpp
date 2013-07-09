9c131f8b0d93697ae19005199fde238a499b8a0e
Jeff Bowles <jbowles@riskybacon.com>
2013-07-09 14:47:39 -0600

    Added a comment clarifying how the view matrix is composed

--- opengl/4.1/shadow_mapping/main.cpp
+++ opengl/4.1/shadow_mapping/main.cpp
@@ -848,6 +848,7 @@ int render(double time)
       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
       GL_ERR_CHECK();
       
+      // view = lookat matrix * eye rotation matrix
       glm::mat4 view = glm::lookAt(vec3(0, 0, 10), vec3(0, 0, 0), vec3(0, 1, 0)) * glm::mat4_cast(_eyeRot);
       
       lightPos = vec4(10, 10, -10, 1);
